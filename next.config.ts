0a39359a11fc6f6c148c5eb0424d711dda14bae8
Henrique Chrispim <henrique.chrispim@protonmail.com>
2026-07-10 21:57:50 -0300

    Fix pipeline

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["192.168.0.14"],
-  // output: "export",
-  // basePath: "/kareoke",
-  // assetPrefix: "/kareoke/",
-  // images: { unoptimized: true },
-  // trailingSlash: true,
+  output: "export",
+  basePath: "/kareoke",
+  assetPrefix: "/kareoke/",
+  images: { unoptimized: true },
+  trailingSlash: true,
 };
 
 export default nextConfig;
